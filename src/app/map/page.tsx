cfbfcfd3169f5a2093ad91cf7b1972eba0ed432d
Corey Mostero <mostero@berkeley.edu>
2024-10-17 20:42:56 -0700

    map change

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,24 +1,34 @@
 import GoogleMapsMapImage from "@/assets/images/google-maps-map.png";
 import VenueMapImage from "@/assets/images/venue-map.png";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
 export default function Map(): React.ReactNode {
 	return (
-		<main className="grid h-screen w-full place-items-center pt-header">
-			<Carousel className="w-3/5">
-				<CarouselContent>
-					<CarouselItem className="grid place-items-center">
-						<Image src={GoogleMapsMapImage} alt="Google maps" />
-					</CarouselItem>
+		<main className="grid h-screen w-full place-items-center">
+			<Tabs className="h-1/2 w-3/4 sm:h-1/2 sm:w-1/2" defaultValue="inside">
+				<TabsList className="grid w-full grid-cols-2 gap-x-4 bg-transparent font-ppmondwest">
+					<TabsTrigger
+						value="inside"
+						className="text-lg hover:border-accent-foreground hover:text-accent-foreground data-[state=active]:border-2 data-[state=active]:border-accent-foreground data-[state=active]:text-accent-foreground sm:w-1/2 sm:justify-self-end"
+					>
+						Inside
+					</TabsTrigger>
+					<TabsTrigger
+						value="outside"
+						className="text-lg hover:border-accent-foreground hover:text-accent-foreground data-[state=active]:border-2 data-[state=active]:border-accent-foreground data-[state=active]:text-accent-foreground sm:w-1/2 sm:justify-self-start"
+					>
+						Outside
+					</TabsTrigger>
+				</TabsList>
+				<TabsContent value="inside" className="grid place-items-center p-4">
+					<Image src={VenueMapImage} alt="Google maps" />
+				</TabsContent>
 
-					<CarouselItem className="grid place-items-center">
-						<Image src={VenueMapImage} alt="Venue map" />
-					</CarouselItem>
-				</CarouselContent>
-				<CarouselPrevious className="h-8 w-8 sm:h-12 sm:w-12" />
-				<CarouselNext className="h-8 w-8 sm:h-12 sm:w-12" />
-			</Carousel>
+				<TabsContent value="outside" className="grid place-items-center p-4">
+					<Image src={GoogleMapsMapImage} alt="Venue map" />
+				</TabsContent>
+			</Tabs>
 		</main>
 	);
 }
--- src/server/routes/live/resources.ts
+++ src/server/routes/live/resources.ts
@@ -1,10 +1,45 @@
 import env from "@/utils/env";
-import { readSpreadsheet } from "@/utils/google";
+import { readSpreadsheet, SheetValues } from "@/utils/google";
+import logger from "@/utils/logger";
+import { z } from "zod";
 
-export async function queryResources() {
+export async function queryResources(): Promise<Resource[]> {
 	const { sheetValues } = await readSpreadsheet(env.SPREADSHEET_ID, "Sponsor & Workshop Starter Packs!A1:D30");
 
 	sheetValues?.shift();
 
-	return sheetValues;
+	return (
+		sheetValues?.reduce((resources: Resource[], values: SheetValues) => {
+			const category = values[0]?.toString();
+			const packName = values[1]?.toString();
+			const url = values[2]?.toString();
+			const imageUrl = values[3]?.toString();
+
+			const resource: Resource = {
+				category: category,
+				name: packName,
+				url: url,
+				imageUrl: imageUrl,
+			};
+
+			const parsedResource = resourceSchema.safeParse(resource);
+			if (!parsedResource.success) {
+				logger.error(parsedResource, parsedResource.error.message);
+				return resources;
+			}
+
+			resources.push(parsedResource.data);
+
+			return resources;
+		}, []) ?? []
+	);
 }
+
+export const resourceSchema = z.object({
+	category: z.optional(z.string()),
+	name: z.string(),
+	url: z.optional(z.string()),
+	imageUrl: z.optional(z.string()),
+});
+
+export type Resource = z.infer<typeof resourceSchema>;
